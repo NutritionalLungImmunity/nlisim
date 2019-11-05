1bb0b04661f263859e28684b96a018dfde7950b3
EricMei <yu.mei@uconn.edu>
2019-11-04 19:55:56 -0500

    preview geometry after init
    Former-commit-id: 65a7b911f5c0e5a1b3d922e3986a009b2262d708

--- simulation/modules/geometry/__init__.py
+++ simulation/modules/geometry/__init__.py
@@ -2,7 +2,9 @@ import attr
 import numpy as np
 import h5py
 import os
-
+import vtk
+import sys
+from vtk.util import numpy_support
 from simulation.module import Module, ModuleState
 from simulation.modules.advection.differences import gradient, laplacian
 from simulation.state import grid_variable, State
@@ -40,6 +42,9 @@ class Geometry(Module):
             raise ValidationError(f'imported geometry doesn\'t match the shape of primary grid')
 
         geometry.geometry_grid = np.copy(g)
+        print(geometry.geometry_grid.shape)
+        
+        self.preview(geometry.geometry_grid)
 
         return state
 
@@ -47,3 +52,81 @@ class Geometry(Module):
         """Advance the state by a single time step."""
         # do nothing since the geoemtry is constant
         return state
+
+    def preview(self, grid: np.ndarray):
+        print(vtk.vtkVersion.GetVTKSourceVersion())
+
+        dataImporter = vtk.vtkImageImport()
+
+        #g = np.frombuffer(self.geo.get_obj())
+        xbin = grid.shape[2]
+        ybin = grid.shape[1]
+        zbin = grid.shape[0]
+        g = np.uint8(grid.reshape(grid.shape[0] * grid.shape[1] * grid.shape[2]))
+        print(g.shape)
+        sys.stdout.flush()
+        data_string = g.tostring()
+        dataImporter.CopyImportVoidPointer(data_string, len(data_string))
+        dataImporter.SetDataScalarTypeToUnsignedChar()
+        dataImporter.SetNumberOfScalarComponents(1)
+
+        dataImporter.SetDataExtent(0, xbin - 1, 0, ybin - 1, 0, zbin - 1)
+        dataImporter.SetWholeExtent(0, xbin - 1, 0,  ybin - 1, 0, zbin - 1)
+
+        # Create transfer mapping scalar value to opacity
+        opacityTransferFunction = vtk.vtkPiecewiseFunction()
+        opacityTransferFunction.AddPoint(0, 0.0)
+        opacityTransferFunction.AddPoint(1, 0.2)
+        opacityTransferFunction.AddPoint(2, 0.005)
+        opacityTransferFunction.AddPoint(3, 1)
+        opacityTransferFunction.AddPoint(4, 0.2)
+        opacityTransferFunction.AddPoint(5, 0.2)
+
+
+        # Create transfer mapping scalar value to color
+        colorTransferFunction = vtk.vtkColorTransferFunction()
+        colorTransferFunction.AddRGBPoint(0, 0.0, 0.0, 1.0)
+        colorTransferFunction.AddRGBPoint(1, 1.0, 0.0, 0.0)
+        colorTransferFunction.AddRGBPoint(2, 0.0, 0.0, 1.0)
+        colorTransferFunction.AddRGBPoint(3, 1.0, 1.0, 1.0)
+        colorTransferFunction.AddRGBPoint(4, 1.0, 1.0, 1.0)
+        colorTransferFunction.AddRGBPoint(5, 1.0, 1.0, 1.0)
+
+        # The property describes how the data will look
+        volumeProperty = vtk.vtkVolumeProperty()
+        volumeProperty.SetColor(colorTransferFunction)
+        volumeProperty.SetScalarOpacity(opacityTransferFunction)
+        #volumeProperty.ShadeOn()
+        volumeProperty.SetInterpolationTypeToLinear()
+
+        # The mapper / ray cast function know how to render the data
+        volumeMapper = vtk.vtkGPUVolumeRayCastMapper()
+        volumeMapper.SetBlendModeToComposite()
+        volumeMapper.SetInputConnection(dataImporter.GetOutputPort())
+
+        # The volume holds the mapper and the property and
+        # can be used to position/orient the volume
+        volume = vtk.vtkVolume()
+        volume.SetMapper(volumeMapper)
+        volume.SetProperty(volumeProperty)
+
+        ren = vtk.vtkRenderer()
+        renWin = vtk.vtkRenderWindow()
+        renWin.AddRenderer(ren)
+        iren = vtk.vtkRenderWindowInteractor()
+        iren.SetRenderWindow(renWin)
+
+        ren.AddVolume(volume)
+        ren.SetBackground(1, 1, 1)
+        renWin.SetSize(600, 600)
+        renWin.Render()
+
+        def CheckAbort(obj, event):
+            if obj.GetEventPending() != 0:
+                obj.SetAbortRender(1)
+
+        renWin.AddObserver("AbortCheckEvent", CheckAbort)
+
+        iren.Initialize()
+        renWin.Render()
+        iren.Start()
