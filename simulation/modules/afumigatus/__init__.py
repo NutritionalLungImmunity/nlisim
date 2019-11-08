0fb2f5fb38466797c100ea747defcf7a589ce26c
Joseph Masison <masison@uchc.edu>
2019-11-08 16:20:45 -0500

    tox changes

--- simulation/modules/afumigatus/__init__.py
+++ simulation/modules/afumigatus/__init__.py
@@ -1,41 +1,35 @@
 """
 Afumigatus Module
 """
-from math import floor
-
 import attr
 import numpy as np
 
 from simulation.module import Module, ModuleState
-from simulation.state import State
-from simulation.validation import ValidationError
 from simulation.modules.afumigatus.afumigatus_cell import AfumigatusCell
-
-from typing import List
-from typing import Dict
+from simulation.state import State
 
 
 @attr.s(kw_only=True, repr=False)
 class AfumigatusState(ModuleState):
-    #Afumigatus data struct
-    lastId:int = attr.ib(default=-1)
+    lastId: int = attr.ib(default=-1)
     min_iter_to_status_change: int = attr.ib(default=0)
     pr_status_change: float = attr.ib(default=0.5)
-	
-    #def __repr__(self):
-    #    return f'AfumigatusState(concentration, wind_x, wind_y, source)'
+
+    # def __repr__(self):
+    #     return f'AfumigatusState(concentration, wind_x, wind_y, source)'
     pass
+
 
 class Afumigatus(Module):
     name = 'afumigatus'
     defaults = {
-        'trees' : 'np.array([])',
-        'min_iter_to_status_change' : '5',
-        'pr_status_change' : '0.05',
-        'num_spore' : '0',
-        'lastId' : '-1'
+        'trees': 'np.array([])',
+        'min_iter_to_status_change': '5',
+        'pr_status_change': '0.05',
+        'num_spore': '0',
+        'lastId': '-1'
     }
-    
+
     StateClass = AfumigatusState
 
     def initialize(self, state: State):
@@ -48,79 +42,84 @@ class Afumigatus(Module):
         afumigatus.pr_status_change = c.getfloat('pr_status_change')
         afumigatus.branch_probability = c.getfloat('branch_probability')
         afumigatus.init_spores = c.getfloat('init_spores')
-        afumigatus.num_spore = 0 
-        afumigatus.last_id = -1 
+        afumigatus.num_spore = 0
+        afumigatus.last_id = -1
 
         return state
 
     def advance(self, state: State, previous_time: float):
         """Advance the state by a single time step."""
         afumigatus: AfumigatusState = state.afumigatus
-        
+
         # load afumigatus data from state
         num_spore = afumigatus.num_spore
         init_spores = afumigatus.init_spores
         trees = afumigatus.trees
-		
-        grid: RectangularGrid = state.grid
+
+        grid: State.RectangularGrid = state.grid
         xbin = grid.shape[2]
         ybin = grid.shape[1]
         zbin = grid.shape[0]
 
-		##iterate over roots and then sub hypheal trees
+        # #iterate over roots and then sub hypheal trees
         for root in trees:
-            nodesToProcess = np.array([])
-            nodesToProcess = np.append(nodesToProcess, root)
-        	
-            while(len(nodesToProcess) > 0):
-                curr_af = nodesToProcess[0]
-                nodesToProcess = np.delete(nodesToProcess, 0, axis=0)
+            nodes_to_process = np.array([])
+            nodes_to_process = np.append(nodes_to_process, root)
+
+            while(len(nodes_to_process) > 0):
+                curr_af = nodes_to_process[0]
+                nodes_to_process = np.delete(nodes_to_process, 0, axis=0)
                 elongate_children = curr_af.elongate_children
                 branch_children = curr_af.branch_children
-               
+
                 if(len(elongate_children) > 0):
                     for c in elongate_children:
-                        nodesToProcess = np.append(nodesToProcess, c)
-                        
+                        nodes_to_process = np.append(nodes_to_process, c)
+
                 if(len(branch_children) > 0):
                     for c in branch_children:
-                        nodesToProcess = np.append(nodesToProcess, c)
-                
-                curr_af.update_status(afumigatus.pr_status_change, afumigatus.min_iter_to_status_change)
+                        nodes_to_process = np.append(nodes_to_process, c)
 
-                #grow, branch
-                new_af = curr_af.elongate(xbin, ybin, zbin) # TODO incorporate geometry growth restriction
+                curr_af.update_status(afumigatus.pr_status_change,
+                                      afumigatus.min_iter_to_status_change)
+
+                # grow, branch
+                # TODO incorporate geometry growth restriction
+                new_af = curr_af.elongate(xbin, ybin, zbin)
                 if(new_af):
                     new_af.id = afumigatus.last_id + 1
                     afumigatus.last_id += 1
                     afumigatus.num_spore += 1
-                new_af2 = curr_af.branch(afumigatus.branch_probability, xbin, ybin, zbin) # TODO incorporate geometry growth restriction
+                # TODO incorporate geometry growth restriction
+                new_af2 = curr_af.branch(afumigatus.branch_probability, xbin, ybin, zbin)
                 if(new_af2):
                     new_af2.id = afumigatus.last_id + 1
                     afumigatus.last_id += 1
                     afumigatus.num_spore += 1
-                if(curr_af.previous_septa == None and not(curr_af.switched) and len(curr_af.elongate_children) > 0):
-                    #we are at root, so can grow opposite direction
+                if(curr_af.previous_septa is None and not(curr_af.switched) and
+                   len(curr_af.elongate_children) > 0):
+                    # we are at root, so can grow opposite direction
                     # TODO add growth delay based on random() < prob_dual elongate
                     curr_af.dx = curr_af.dx * - 1
                     curr_af.dy = curr_af.dy * - 1
                     curr_af.dz = curr_af.dz * - 1
                     curr_af.growable = True
                     curr_af.switched = True
-                
-		##add new spores if haven't inhaled the limit
-        if (num_spore < init_spores):
+
+        # #add new spores if haven't inhaled the limit
+        if(num_spore < init_spores):
             # TODO add limitation to just lodge layer eg:
             # rz, ry, rx = np.where(geometry==SPORE_LAYER) # macro read from config
             while(afumigatus.num_spore < init_spores):
-                #index = randint(0, len(rz))
+                # index = randint(0, len(rz))
                 afumigatus.last_id += 1
                 last_id = afumigatus.last_id
-                af = AfumigatusCell(x=10, y=10, z=10, ironPool = 0, status = AfumigatusCell.Status.RESTING_CONIDIA,\
-                     state = AfumigatusCell.State.FREE, isRoot = True, id_in = last_id)
+                af = AfumigatusCell(x=10, y=10, z=10, ironPool=0,
+                                    status=AfumigatusCell.Status.RESTING_CONIDIA,
+                                    state=AfumigatusCell.State.FREE, isRoot=True, id_in=last_id)
                 afumigatus.trees = np.append(afumigatus.trees, af)
                 afumigatus.num_spore += 1
-        
-        # TODO diffuseIron()	
+
+        # TODO diffuseIron()
 
         return state
--- simulation/modules/afumigatus/afumigatus_cell.py
+++ simulation/modules/afumigatus/afumigatus_cell.py
@@ -1,14 +1,18 @@
-from simulation.cell_lib.cell import Cell
-from random import random, uniform
 from enum import IntEnum
-import numpy as np
 import math
+from random import random, uniform
+
+import numpy as np
+
+from simulation.cell_lib.cell import Cell
+
 
 class AfumigatusCell(Cell):
-    name = "AfumigatusCell"
-    InitAfumigatusBooleanState = [1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    
-    #status:
+    name = 'AfumigatusCell'
+    InitAfumigatusBooleanState = [
+        1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    # status:
     class Status(IntEnum):
         RESTING_CONIDIA = 0
         SWELLING_CONIDIA = 1
@@ -16,18 +20,19 @@ class AfumigatusCell(Cell):
         DYING = 3
         DEAD = 4
 
-    #state:
+    # state:
     class State(IntEnum):
         FREE = 0
         INTERNALIZING = 1
         RELEASING = 2
 
-    def __init__(self, x=0, y=0, z=0, ironPool = 0, status = Status.RESTING_CONIDIA, state = State.FREE, isRoot = True, id_in = -1):
+    def __init__(self, x=0, y=0, z=0, iron_pool=0, status=Status.RESTING_CONIDIA,
+                 state=State.FREE, is_root=True, id_in=-1):
         self.id = id_in
-        self.iron_pool = ironPool
+        self.iron_pool = iron_pool
         self.state = state
         self.status = status
-        self.is_root = isRoot
+        self.is_root = is_root
         self.x = x
         self.y = y
         self.z = z
@@ -47,7 +52,7 @@ class AfumigatusCell(Cell):
         self.Fe = False
 
     def __str__(self):
-        s =  "AfumigatusCell_" + str(self.id) + \
+        s = 'AfumigatusCell_' + str(self.id) + \
             ' x=' + '%.5f' % self.x + \
             ' y=' + '%.5f' % self.y + \
             ' z=' + '%.5f' % self.z + \
@@ -55,33 +60,33 @@ class AfumigatusCell(Cell):
             ' dy=' + '%.5f' % self.dy + \
             ' dz=' + '%.5f' % self.dz
 
-        if(self.status == Status.RESTING_CONIDIA):
-            s = s + ' RESTING_CONIDIA'
-        if(self.status == Status.SWELLING_CONIDIA):
-            s = s + ' SWELLING_CONIDIA'
-        if(self.status == Status.HYPHAE):
-            s = s + ' HYPHAE'
-        if(self.status == Status.DYING):
-            s = s + ' DYING'
-        if(self.status == Status.DEAD):
-            s = s + ' DEAD'
-        if(self.state == State.FREE):
-            s = s + ' FREE'
-        if(self.state == State.INTERNALIZING):
-            s = s + ' INTERNALIZING'
-        if(self.state == State.RELEASING):
-            s = s + ' RELEASING'
-       
-        if(self.previous_septa):
-            s = s + ' prev=' + str(self.previous_septa.id)
-        if(len(self.branch_children) > 0):
-            s  = s + ' b_children= ' + str(self.branch_children[0].id)
-            for c in self.branch_children[1:]:
-                s = s + ', ' + str(c.id)
-        if(len(self.elongate_children) > 0):
-            s  = s + ' e_children= ' + str(self.elongate_children[0].id)
-            for c in self.elongate_children[1:]:
-                s = s + ', ' + str(c.id)
+        #    if(self.status == AfumigatusCell.Status.RESTING_CONIDIA):
+        #        s = s + ' RESTING_CONIDIA'
+        #    if(self.status == AfumigatusCell.Status.SWELLING_CONIDIA):
+        #        s = s + ' SWELLING_CONIDIA'
+        #    if(self.status == AfumigatusCell.Status.HYPHAE):
+        #        s = s + ' HYPHAE'
+        #    if(self.status == AfumigatusCell.Status.DYING):
+        #        s = s + ' DYING'
+        #    if(self.status == AfumigatusCell.Status.DEAD):
+        #        s = s + ' DEAD'
+        #    if(self.state == AfumigatusCell.State.FREE):
+        #        s = s + ' FREE'
+        #    if(self.state == AfumigatusCell.State.INTERNALIZING):
+        #        s = s + ' INTERNALIZING'
+        #    if(self.state == AfumigatusCell.State.RELEASING):
+        #        s = s + ' RELEASING'
+
+        # if(self.previous_septa):
+        #     s = s + ' prev=' + str(self.previous_septa.id)
+        # if(len(self.branch_children) > 0):
+        #     s = s + ' b_children= ' + str(self.branch_children[0].id)
+        #     for c in self.branch_children[1:]:
+        #         s = s + ', ' + str(c.id)
+        # if(len(self.elongate_children) > 0):
+        #     s = s + ' e_children= ' + str(self.elongate_children[0].id)
+        #     for c in self.elongate_children[1:]:
+        #         s = s + ', ' + str(c.id)
         return s
 
     def set_growth_vector(self, growth_vector):
@@ -89,17 +94,20 @@ class AfumigatusCell(Cell):
         self.dy = growth_vector[1]
         self.dz = growth_vector[2]
 
-    def elongate(self, xbin, ybin, zbin): # TODO incorporate geometry growth restriction
-        if self.growable and self.status == Status.HYPHAE:# TODO add iron dependence and self.boolean_network[AfumigatusCell.LIP] == 1:
-            if(self.x + self.dx < 0 or self.y + self.dy < 0 or self.z + self.dz < 0 \
-                or self.x + self.dx > xbin or self.y + self.dy > ybin or self.z + self.dz > zbin):
+    def elongate(self, xbin, ybin, zbin):
+        # TODO incorporate geometry growth restriction
+        if self.growable and self.status == AfumigatusCell.Status.HYPHAE:
+            # TODO add iron dependence and self.boolean_network[AfumigatusCell.LIP] == 1:
+            if(self.x + self.dx < 0 or self.y + self.dy < 0 or self.z + self.dz < 0
+               or self.x + self.dx > xbin or self.y + self.dy > ybin or self.z + self.dz > zbin):
                 return None
             else:
                 self.growable = False
                 self.branchable = True
-                self.iron_pool = self.iron_pool / 2.0;
-                child = AfumigatusCell(x=self.x + self.dx, y=self.y + self.dy, z=self.z + self.dz,\
-                                             ironPool=0, status=Status.HYPHAE, state=self.state, isRoot=False)
+                self.iron_pool = self.iron_pool / 2.0
+                child = AfumigatusCell(x=self.x + self.dx, y=self.y + self.dy, z=self.z + self.dz,
+                                       iron_pool=0, status=AfumigatusCell.Status.HYPHAE,
+                                       state=self.state, is_root=False)
                 child.previous_septa = self
                 child.iron_pool = self.iron_pool
                 child.is_root = False
@@ -107,79 +115,79 @@ class AfumigatusCell(Cell):
                 return child
         return None
 
-    def branch(self, branch_probability, xbin, ybin, zbin): # TODO incorporate geometry growth restriction
-        if self.branchable and self.status == AfumigatusCell.HYPHAE: # TODO add iron dependence  and self.boolean_network[AfumigatusCell.LIP] == 1:
+    def branch(self, branch_probability, xbin, ybin, zbin):
+        # TODO incorporate geometry growth restriction
+        if self.branchable and self.status == AfumigatusCell.Status.HYPHAE:
+            # TODO add iron dependence  and self.boolean_network[AfumigatusCell.LIP] == 1:
             if random() < branch_probability:
                 self.iron_pool = self.iron_pool / 2.0
                 growth_vector = [self.dx, self.dy, self.dz]
-                B = np.array([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], growth_vector])
-                B_inv = np.linalg.inv(B)
-                R = rotatation_matrix(2*random()*math.pi)
-                R = np.dot(B, np.dot(R, B_inv))
-                growth_vector = np.dot(R, growth_vector)
-                
-                nextX = growth_vector[0] + self.x
-                nextY = growth_vector[1] + self.y
-                nextZ = growth_vector[2] + self.z
+                b = np.array([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], growth_vector])
+                b_inv = np.linalg.inv(b)
+                r = AfumigatusCell.rotatation_matrix(2*random()*math.pi)
+                r = np.dot(b, np.dot(r, b_inv))
+                growth_vector = np.dot(r, growth_vector)
 
-                if(nextX < 0 or nextY < 0 or nextZ < 0 \
-                or nextX > xbin or nextY > ybin or nextZ > zbin):
+                next_x = growth_vector[0] + self.x
+                next_y = growth_vector[1] + self.y
+                next_z = growth_vector[2] + self.z
+
+                if(next_x < 0 or next_y < 0 or next_z < 0
+                   or next_x > xbin or next_y > ybin or next_z > zbin):
                     return None
                 else:
-                    child = AfumigatusCell(x=nextX, y=nextY, z=nextZ,\
-                                                ironPool=0, status=Status.HYPHAE, \
-                                                state=self.state, isRoot=False)
+                    child = AfumigatusCell(x=next_x, y=next_y, z=next_z,
+                                           iron_pool=0, status=AfumigatusCell.Status.HYPHAE,
+                                           state=self.state, is_root=False)
                     child.set_growth_vector(growth_vector)
                     child.iron_pool = self.iron_pool
                     child.previous_septa = self
                     child.is_root = False
                     self.branch_children = np.append(self.branch_children, child)
-                    
-                    #self.branchable = False    
-                    ##set neighbors to be unbranchable
-                    #self.next_branch.branchable = False
-                    #if(self.previous_septa):
+
+                    # self.branchable = False
+                    # # set neighbors to be unbranchable
+                    # self.next_branch.branchable = False
+                    # if(self.previous_septa):
                     #    self.previous_septa.branchable = False
 
                     return child
-                #self.branchable = False
+                # self.branchable = False
         return None
 
     def update_status(self, probability_status_change, min_iter_to_status_change):
         self.iteration = self.iteration + 1
-        if self.status == Status.RESTING_CONIDIA and \
+        if self.status == AfumigatusCell.Status.RESTING_CONIDIA and \
                 self.iteration >= min_iter_to_status_change and \
                 random() < probability_status_change:
-            self.status = Status.SWELLING_CONIDIA
+            self.status = AfumigatusCell.Status.SWELLING_CONIDIA
             self.iteration = 0
-        elif self.status == Status.SWELLING_CONIDIA and \
+        elif self.status == AfumigatusCell.Status.SWELLING_CONIDIA and \
                 self.iteration >= min_iter_to_status_change and \
                 random() < probability_status_change:
-            self.status = Status.HYPHAE
+            self.status = AfumigatusCell.Status.HYPHAE
             self.iteration = 0
-        elif self.status == Status.DYING:
-            self.status = Status.DEAD
+        elif self.status == AfumigatusCell.Status.DYING:
+            self.status = AfumigatusCell.Status.DEAD
 
     def is_dead(self):
-        return self.status == Status.DEAD
+        return self.status == AfumigatusCell.Status.DEAD
 
     def leave(self, qtty):
         return False
 
-    def move(self, oldVoxel, newVoxel):
+    def move(self, old_voxel, new_voxel):
         pass
 
     def process_boolean_network(self):
         pass
 
-    
     @staticmethod
     def rotatation_matrix(phi):
-        cosTheta = math.cos(math.pi/4.0)
-        sinTheta = math.sin(math.pi / 4.0)
+        cos_theta = math.cos(math.pi/4.0)
+        sin_theta = math.sin(math.pi / 4.0)
 
-        return np.array([[cosTheta * math.cos(phi), \
-            -cosTheta*math.sin(phi), sinTheta], \
-            [math.sin(phi), math.cos(phi), 0.0],\
-            [-sinTheta*math.cos(phi), sinTheta*math.sin(phi), cosTheta]])
-
+        return np.array([
+            [cos_theta * math.cos(phi), -cos_theta*math.sin(phi), sin_theta],
+            [math.sin(phi), math.cos(phi), 0.0],
+            [-sin_theta*math.cos(phi), sin_theta*math.sin(phi), cos_theta]])
