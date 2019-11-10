145358ce20099cc250de57f36c31378169c1b890
EricMei <yu.mei@uconn.edu>
2019-11-09 22:38:09 -0500

    Fixed some format issues
    Former-commit-id: 28783ef463fd086574d6cd9c3ac950e0b1b054dd

--- simulation/modules/geometry/__init__.py
+++ simulation/modules/geometry/__init__.py
@@ -1,13 +1,14 @@
-import attr
-import numpy as np
-import h5py
-import os
-import vtk
 from enum import Enum
+
+import attr
+import h5py
+import numpy as np
+import vtk
 
 from simulation.module import Module, ModuleState
 from simulation.state import grid_variable, State
 from simulation.validation import ValidationError
+
 
 # I am not quite sure if we should put the definition of the lung tissue types here
 class TissueTypes(Enum):
@@ -20,8 +21,10 @@ class TissueTypes(Enum):
 
     @classmethod
     def validate(cls, value: np.ndarray):
-        return np.logical_and(value >= 0, value <= 5).all() and np.issubclass_(value.dtype.type, np.integer)
-        
+        return np.logical_and(value >= 0, value <= 5).all()\
+                and np.issubclass_(value.dtype.type, np.integer)
+
+
 @attr.s(kw_only=True, repr=False)
 class GeometryState(ModuleState):
     lung_tissue = grid_variable(np.dtype('int'))
@@ -50,7 +53,7 @@ class Geometry(Module):
         try:
             with h5py.File(path, 'r') as f:
                 if f['geometry'][:].shape != state.grid.shape:
-                    raise ValidationError('imported geometry doesn\'t match the shape of primary grid')
+                    raise ValidationError("shape doesn\'t match")
                 geometry.lung_tissue[:] = f['geometry'][:]
         except Exception:
             print(f'Error loading geometry file at {path}.')
@@ -58,23 +61,20 @@ class Geometry(Module):
 
         if preview_geometry:
             Geometry.preview(geometry.lung_tissue)
-            #the pragram will be blocked even a new thread is created. maybe a conflict of the popping window?
-            #thread = threading.Thread(target = Geometry.preview, args = (geometry.lung_tissue, ))
-            #thread.start()
-            
+
         return state
 
-    # same as the super class. not needed for now
-    # def advance(self, state: State, previous_time: float):
-    #     """Advance the state by a single time step."""
-    #     # do nothing since the geoemtry is constant
-    #     return state
+    '''
+    same as the super class. not needed for now
+    def advance(self, state: State, previous_time: float):
+        """Advance the state by a single time step."""
+        # do nothing since the geoemtry is constant
+        return state
+    '''
 
     @classmethod
     def preview(cls, grid: np.ndarray):
-        #print(vtk.vtkVersion.GetVTKSourceVersion())
-        #sys.stdout.flush()
-        dataImporter = vtk.vtkImageImport()
+        data_importer = vtk.vtkImageImport()
 
         xbin = grid.shape[2]
         ybin = grid.shape[1]
@@ -82,67 +82,66 @@ class Geometry(Module):
         g = np.uint8(grid.reshape(grid.shape[0] * grid.shape[1] * grid.shape[2]))
 
         data_string = g.tostring()
-        dataImporter.CopyImportVoidPointer(data_string, len(data_string))
-        dataImporter.SetDataScalarTypeToUnsignedChar()
-        dataImporter.SetNumberOfScalarComponents(1)
+        data_importer.CopyImportVoidPointer(data_string, len(data_string))
+        data_importer.SetDataScalarTypeToUnsignedChar()
+        data_importer.SetNumberOfScalarComponents(1)
 
-        dataImporter.SetDataExtent(0, xbin - 1, 0, ybin - 1, 0, zbin - 1)
-        dataImporter.SetWholeExtent(0, xbin - 1, 0,  ybin - 1, 0, zbin - 1)
+        data_importer.SetDataExtent(0, xbin - 1, 0, ybin - 1, 0, zbin - 1)
+        data_importer.SetWholeExtent(0, xbin - 1, 0,  ybin - 1, 0, zbin - 1)
 
         # Create transfer mapping scalar value to opacity
-        opacityTransferFunction = vtk.vtkPiecewiseFunction()
-        opacityTransferFunction.AddPoint(0, 0.0)
-        opacityTransferFunction.AddPoint(1, 0.2)
-        opacityTransferFunction.AddPoint(2, 0.005)
-        opacityTransferFunction.AddPoint(3, 1)
-        opacityTransferFunction.AddPoint(4, 0.2)
-        opacityTransferFunction.AddPoint(5, 0.2)
-
+        opacity_transfer_function = vtk.vtkPiecewiseFunction()
+        opacity_transfer_function.AddPoint(0, 0.0)
+        opacity_transfer_function.AddPoint(1, 0.2)
+        opacity_transfer_function.AddPoint(2, 0.005)
+        opacity_transfer_function.AddPoint(3, 1)
+        opacity_transfer_function.AddPoint(4, 0.2)
+        opacity_transfer_function.AddPoint(5, 0.2)
 
         # Create transfer mapping scalar value to color
-        colorTransferFunction = vtk.vtkColorTransferFunction()
-        colorTransferFunction.AddRGBPoint(0, 0.0, 0.0, 1.0)
-        colorTransferFunction.AddRGBPoint(1, 1.0, 0.0, 0.0)
-        colorTransferFunction.AddRGBPoint(2, 0.0, 0.0, 1.0)
-        colorTransferFunction.AddRGBPoint(3, 1.0, 1.0, 1.0)
-        colorTransferFunction.AddRGBPoint(4, 1.0, 1.0, 1.0)
-        colorTransferFunction.AddRGBPoint(5, 1.0, 1.0, 1.0)
+        color_transfer_function = vtk.vtkColorTransferFunction()
+        color_transfer_function.AddRGBPoint(0, 0.0, 0.0, 1.0)
+        color_transfer_function.AddRGBPoint(1, 1.0, 0.0, 0.0)
+        color_transfer_function.AddRGBPoint(2, 0.0, 0.0, 1.0)
+        color_transfer_function.AddRGBPoint(3, 1.0, 1.0, 1.0)
+        color_transfer_function.AddRGBPoint(4, 1.0, 1.0, 1.0)
+        color_transfer_function.AddRGBPoint(5, 1.0, 1.0, 1.0)
 
         # The property describes how the data will look
-        volumeProperty = vtk.vtkVolumeProperty()
-        volumeProperty.SetColor(colorTransferFunction)
-        volumeProperty.SetScalarOpacity(opacityTransferFunction)
-        #volumeProperty.ShadeOn()
-        volumeProperty.SetInterpolationTypeToLinear()
+        volume_property = vtk.vtkVolumeProperty()
+        volume_property.SetColor(color_transfer_function)
+        volume_property.SetScalarOpacity(opacity_transfer_function)
+        # volumeProperty.ShadeOn()
+        volume_property.SetInterpolationTypeToLinear()
 
         # The mapper / ray cast function know how to render the data
-        volumeMapper = vtk.vtkGPUVolumeRayCastMapper()
-        volumeMapper.SetBlendModeToComposite()
-        volumeMapper.SetInputConnection(dataImporter.GetOutputPort())
+        volume_mapper = vtk.vtkGPUVolumeRayCastMapper()
+        volume_mapper.SetBlendModeToComposite()
+        volume_mapper.SetInputConnection(data_importer.GetOutputPort())
 
         # The volume holds the mapper and the property and
         # can be used to position/orient the volume
         volume = vtk.vtkVolume()
-        volume.SetMapper(volumeMapper)
-        volume.SetProperty(volumeProperty)
+        volume.SetMapper(volume_mapper)
+        volume.SetProperty(volume_property)
 
         ren = vtk.vtkRenderer()
-        renWin = vtk.vtkRenderWindow()
-        renWin.AddRenderer(ren)
+        ren_win = vtk.vtkRenderWindow()
+        ren_win.AddRenderer(ren)
         iren = vtk.vtkRenderWindowInteractor()
-        iren.SetRenderWindow(renWin)
+        iren.SetRenderWindow(ren_win)
 
         ren.AddVolume(volume)
         ren.SetBackground(1, 1, 1)
-        renWin.SetSize(600, 600)
-        renWin.Render()
+        ren_win.SetSize(600, 600)
+        ren_win.Render()
 
-        def CheckAbort(obj, event):
+        def check_abort(obj, event):
             if obj.GetEventPending() != 0:
                 obj.SetAbortRender(1)
 
-        renWin.AddObserver("AbortCheckEvent", CheckAbort)
+        ren_win.AddObserver('AbortCheckEvent', check_abort)
 
         iren.Initialize()
-        renWin.Render()
+        ren_win.Render()
         iren.Start()
