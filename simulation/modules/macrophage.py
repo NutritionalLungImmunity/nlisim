2bce5c2e72d0451db4edec823a68c2ca3294721f
Joseph Masison <masison@uchc.edu>
2019-12-02 14:12:03 -0500

    surfactant init

--- simulation/modules/macrophage.py
+++ simulation/modules/macrophage.py
@@ -1,9 +1,10 @@
 from enum import IntEnum
 
 import attr
+import random
 import numpy as np
 
-from simulation.cell import CellList
+from simulation.cell import CellList, CellData
 from simulation.coordinates import Point
 from simulation.state import State, RectangularGrid
 from simulation.module import Module, ModuleState
@@ -29,7 +30,7 @@ class MacrophageCellData(CellData):
         ('iteration', 'i4'),
     ]
 
-    dtype = np.dtype(CellList.BASE_FIELDS + MACROPHAGE_FIELDS, align=True)  # type: ignore
+    dtype = np.dtype(CellData.BASE_FIELDS + MACROPHAGE_FIELDS, align=True)  # type: ignore
 
     @classmethod
     def create_cell(
@@ -101,27 +102,37 @@ class MacrophageState(ModuleState):
 
 class Macrophage(Module):
     name = 'macrophage'
+    defaults = {
+        'cells': '',
+        'init_num': '0',
+    }
     StateClass = MacrophageState
 
     def initialize(self, state: State):
         macrophage: MacrophageState = state.macrophage
         grid: RectangularGrid = state.grid
-
+        tissue: GeometryState = state.geometry.lung_tissue
 
         # macrophage.recruit_rate = self.config.getfloat('rec_rate')
         macrophage.init_num = self.config.getint('init_num')
 
+        indices = np.argwhere(tissue == TissueTypes.SURFACTANT)
+        np.random.shuffle(indices)
 
-        if(macrophage.init_num > 0):
-            # create one cell 
-            macrophage.cells = MacrophageCellList.create_from_seed(grid=grid, point=point, status=MacrophageCellData.Status.RESTING)
+        for i in range(0, macrophage.init_num):
+            x = indices[i][2] + (random.uniform(-0.5, 0.5))
+            y = indices[i][1] + (random.uniform(-0.5, 0.5))
+            z = indices[i][0] + (random.uniform(-0.5, 0.5))
+            point = Point(x=x, y=y, z=z)
+
+            if(i == 0):
+                # create one cell
+                macrophage.cells = MacrophageCellList.create_from_seed(grid=grid, point=point, status=MacrophageCellData.Status.RESTING)
+            else:
+                macrophage.cells.append(MacrophageCellData.create_cell(point=point, status=MacrophageCellData.Status.RESTING))
 
         return state
 
     def advance(self, state: State, previous_time: float):
-
         return state
-
-    def get_surfactant_layer(self):
-        return 
 
