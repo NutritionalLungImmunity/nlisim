c8702ff91211372c81785481b2c70c05996df1f0
Eric Mei <ericmei@umich.edu>
2019-12-29 19:25:43 -0500

    visualize alive cells only

--- simulation/modules/visualization/__init__.py
+++ simulation/modules/visualization/__init__.py
@@ -1,27 +1,26 @@
-from math import floor
-from time import time
 from enum import Enum
 
 import attr
 import numpy as np
-import pylab
 import vtk
 from vtk.util import numpy_support
 
+from simulation.cell import CellList
 from simulation.config import SimulationConfig
 from simulation.module import Module, ModuleState
+from simulation.modules.afumigatus import AfumigatusCellTreeList
 from simulation.state import State
-from simulation.cell import CellArray, CellTree
-from simulation.coordinates import Point
 
 
 class VTKTypes(Enum):
-    '''a enum class for the vtk data type'''
+    """a enum class for the vtk data type."""
+
     STRUCTURED_POINTS = 0
     STRUCTURED_GRID = 1
     RECTILINEAR_GRID = 2
     UNSTRUCTURED_GRID = 3
     POLY_DATA = 4
+
 
 @attr.s(kw_only=True, repr=False)
 class VisualizationState(ModuleState):
@@ -29,6 +28,7 @@ class VisualizationState(ModuleState):
 
     def __repr__(self):
         return 'VisualizationState(last_visualize)'
+
 
 class Visualization(Module):
     name = 'visualization'
@@ -44,10 +44,9 @@ class Visualization(Module):
         verts = vtk.vtkPoints()
         lines = vtk.vtkCellArray()
 
-        # Currently, only CellTree is supported. Could add more datatype in the futrue
-        if (isinstance(var, CellTree)):
-            cells = var.cells # get cell array
-            for cell in cells:
+        if (isinstance(var, AfumigatusCellTreeList)):
+            for index in var.cells.alive():
+                cell = var.cells[index]
                 verts.InsertNextPoint(cell['point'][2], cell['point'][1], cell['point'][0])
 
             adjacency = var.adjacency
@@ -59,8 +58,14 @@ class Visualization(Module):
                     line.GetPointIds().SetId(1, j)
                     lines.InsertNextCell(line)
 
+        elif (isinstance(var, CellList)):
+            for index in var.cells.alive():
+                cell = var.cells[index]
+                verts.InsertNextPoint(cell['point'][2], cell['point'][1], cell['point'][0])
+
         else:
-            raise NotImplementedError(f'Only supported CellTree for POLY_DATA. Got {type(var)}')
+            raise NotImplementedError(f'Only supported CellTree or CellList for POLY_DATA. \
+                Got {type(var)}')
 
         vol.SetPoints(verts)
         vol.SetLines(lines)
@@ -71,8 +76,8 @@ class Visualization(Module):
 
     @classmethod
     def write_structured_points(cls, var: np.ndarray, filename: str,
-                                dx: float = 0.1, 
-                                dy: float = 0.1, 
+                                dx: float = 0.1,
+                                dy: float = 0.1,
                                 dz: float = 0.1) -> None:
 
         vol = vtk.vtkStructuredPoints()
@@ -82,16 +87,13 @@ class Visualization(Module):
         vol.SetOrigin(0, 0, 0)
         vol.SetSpacing(dx, dy, dz)
 
-        scalars = numpy_support.numpy_to_vtk(num_array = var.ravel())
-        #scalars = scalars.reshape(var.shape)
+        scalars = numpy_support.numpy_to_vtk(num_array=var.ravel())
 
         vol.GetPointData().SetScalars(scalars)
         writer = vtk.vtkStructuredPointsWriter()
         writer.SetFileName(filename)
         writer.SetInputData(vol)
         writer.Write()
-
-
 
     @classmethod
     def visualize(cls, state: State, variable: str, filename: str) -> None:
@@ -122,7 +124,7 @@ class Visualization(Module):
         visualize_interval = self.config.getfloat('visualize_interval')
         visualization_file_name = self.config.get('visualization_file_name')
         variables = SimulationConfig.parselist(self.config.get('visual_variables'))
-        now = state.time;
+        now = state.time
 
         if now - state.visualization.last_visualize > visualize_interval - 1e-8:
             for variable in variables:
