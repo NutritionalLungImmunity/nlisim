5e72793b9d90662fa2b205f9d17e0afbb523a18b
Eric Mei <ericmei@umich.edu>
2019-12-31 19:25:51 -0500

    visualize selected attribute

--- simulation/modules/visualization/__init__.py
+++ simulation/modules/visualization/__init__.py
@@ -1,15 +1,15 @@
 from enum import Enum
+import json
 
 import attr
 import numpy as np
 import vtk
-from vtk.util import numpy_support
 
 from simulation.cell import CellList
-from simulation.config import SimulationConfig
 from simulation.module import Module, ModuleState
 from simulation.modules.afumigatus import AfumigatusCellTreeList
 from simulation.state import State
+from simulation.vtk_support import numpy_to_vtk
 
 
 class VTKTypes(Enum):
@@ -34,17 +34,17 @@ class Visualization(Module):
     name = 'visualization'
     defaults = {
         'visualize_interval': '1',
-        'visualization_file_name': 'output/<variable>-<time>.vtk'
+        'visualization_file_name': 'output/<variable>-<time>.vtk',
     }
     StateClass = VisualizationState
 
     @classmethod
-    def write_poly_data(cls, var, filename: str) -> None:
+    def write_poly_data(cls, var, filename: str, attr_names: str) -> None:
         vol = vtk.vtkPolyData()
         verts = vtk.vtkPoints()
         lines = vtk.vtkCellArray()
 
-        if (isinstance(var, AfumigatusCellTreeList)):
+        if isinstance(var, AfumigatusCellTreeList):
             for index in var.cells.alive():
                 cell = var.cells[index]
                 verts.InsertNextPoint(cell['point'][2], cell['point'][1], cell['point'][0])
@@ -58,14 +58,22 @@ class Visualization(Module):
                     line.GetPointIds().SetId(1, j)
                     lines.InsertNextCell(line)
 
-        elif (isinstance(var, CellList)):
-            for index in var.cells.alive():
-                cell = var.cells[index]
-                verts.InsertNextPoint(cell['point'][2], cell['point'][1], cell['point'][0])
+        elif not isinstance(var, CellList):
+            raise NotImplementedError(
+                f'Only supported CellTree or CellList for POLY_DATA. \
+                Got {type(var)}'
+            )
 
-        else:
-            raise NotImplementedError(f'Only supported CellTree or CellList for POLY_DATA. \
-                Got {type(var)}')
+        for index in var.alive():
+            cell = var[index]
+            verts.InsertNextPoint(cell['point'][2], cell['point'][1], cell['point'][0])
+
+        alive_cells = np.take(var.cell_data, var.alive())
+        for attr_name in attr_names:
+            attr = alive_cells[attr_name]
+            scalars = numpy_to_vtk(num_array=attr)
+            scalars.SetName(attr_name)
+            vol.GetPointData().AddArray(scalars)
 
         vol.SetPoints(verts)
         vol.SetLines(lines)
@@ -75,10 +83,9 @@ class Visualization(Module):
         writer.Write()
 
     @classmethod
-    def write_structured_points(cls, var: np.ndarray, filename: str,
-                                dx: float = 0.1,
-                                dy: float = 0.1,
-                                dz: float = 0.1) -> None:
+    def write_structured_points(
+        cls, var: np.ndarray, filename: str, dx: float = 0.1, dy: float = 0.1, dz: float = 0.1
+    ) -> None:
 
         vol = vtk.vtkStructuredPoints()
 
@@ -87,7 +94,7 @@ class Visualization(Module):
         vol.SetOrigin(0, 0, 0)
         vol.SetSpacing(dx, dy, dz)
 
-        scalars = numpy_support.numpy_to_vtk(num_array=var.ravel())
+        scalars = numpy_to_vtk(num_array=var.ravel())
 
         vol.GetPointData().SetScalars(scalars)
         writer = vtk.vtkStructuredPointsWriter()
@@ -95,10 +102,11 @@ class Visualization(Module):
         writer.SetInputData(vol)
         writer.Write()
 
-    @classmethod
-    def visualize(cls, state: State, variable: str, filename: str) -> None:
-        variable_name, vtk_type = variable.split('|')
-        module_name, var_name = variable_name.split('.')
+    def visualize(self, state: State, json_config: dict, filename: str) -> None:
+        module_name = json_config['module']
+        var_name = json_config['variable']
+        vtk_type = json_config['vtk_type']
+        attr_names = json_config['attributes']
         file_name = filename.replace('<variable>', module_name + '-' + var_name)
         var = getattr(getattr(state, module_name), var_name)
 
@@ -106,7 +114,7 @@ class Visualization(Module):
             Visualization.write_structured_points(var, file_name)
 
         elif vtk_type == VTKTypes.POLY_DATA.name:
-            Visualization.write_poly_data(var, file_name)
+            Visualization.write_poly_data(var, file_name, attr_names)
 
         elif vtk_type == VTKTypes.STRUCTURED_GRID.name:
             raise NotImplementedError('structred_grid is not supported yet')
@@ -123,11 +131,12 @@ class Visualization(Module):
     def advance(self, state: State, previous_time: float) -> State:
         visualize_interval = self.config.getfloat('visualize_interval')
         visualization_file_name = self.config.get('visualization_file_name')
-        variables = SimulationConfig.parselist(self.config.get('visual_variables'))
+        variables = self.config.get('visual_variables')
+        json_config = json.loads(variables)
         now = state.time
 
         if now - state.visualization.last_visualize > visualize_interval - 1e-8:
-            for variable in variables:
+            for variable in json_config:
                 file_name = visualization_file_name.replace('<time>', ('%010.3f' % now).strip())
                 self.visualize(state, variable, file_name)
                 state.visualization.last_visualize = now
