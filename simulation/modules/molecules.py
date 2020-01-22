1ede0aaeec917b76d76d1e5ab6ef3c66ebb36242
Eric Mei <yu.mei@uconn.edu>
2020-01-21 23:28:38 -0500

    Change molecule initial state by config

--- simulation/modules/molecules.py
+++ simulation/modules/molecules.py
@@ -1,11 +1,16 @@
-from enum import Enum
+from enum import Enum, unique
+import json
 
 import attr
 import numpy as np
 
 from simulation.module import Module, ModuleState
+from simulation.modules.geometry import GeometryState, TissueTypes
 from simulation.state import grid_variable, State
+from simulation.validation import ValidationError
 
+
+@unique
 class MoleculeTypes(Enum):
     """a enum class for the molecule type."""
 
@@ -30,14 +35,22 @@ class MoleculeTypes(Enum):
 
 @attr.s(kw_only=True, repr=False)
 class MoleculesState(ModuleState):
-    concentration = grid_variable(dtype = {
-                    'names':[molecule.name for molecule in MoleculeTypes],
-                    'formats':['f4'] * len(MoleculeTypes)
-                })
+    concentration = grid_variable(
+        dtype={
+            'names': [molecule.name for molecule in MoleculeTypes],
+            'formats': ['f4'] * len(MoleculeTypes),
+        }
+    )
 
     # we may want to identify sources of molecules in the tissue e.g. blood
-    #  that preferentially increase at a time step
+    # that preferentially increase at a time step
     # source = grid_variable()
+
+    @concentration.validator
+    def _validate_concentration(self, attribute: attr.Attribute, value: np.ndarray) -> None:
+        for name in value.dtype.names:
+            if not np.greater_equal(value[name], 0).all():
+                raise ValidationError('concentration must >= 0')
 
     def __repr__(self):
         return f'MoleculesState(iron)'
@@ -45,28 +58,6 @@ class MoleculesState(ModuleState):
 
 class Molecules(Module):
     name = 'molecules'
-    '''
-    defaults = {
-        'iron': '',
-        'tf': '',
-        'tfbi': '',
-        'tafc': '',
-        'tafcbi': '',
-        'ros': '',
-        'lactoferrin': '',
-        'lactoferrinbi': '',
-        'il6': '',
-        'tnfa': '',
-        'il8': '',
-        'il10': '',
-        'hepcidin': '',
-        'tgfb': '',
-        'mcp1': '',
-        'mip2': '',
-        'mip1b': '',
-    }
-    '''
-
     StateClass = MoleculesState
 
     def initialize(self, state: State):
@@ -77,25 +68,38 @@ class Molecules(Module):
         if not np.any(geometry.lung_tissue):
             raise RuntimeError('geometry molecule has to be initialized first')
 
+        molecules_config = self.config.get('molecules')
+        json_config = json.loads(molecules_config)
+
+        for molecule in json_config:
+            name = molecule['name']
+            init_val = molecule['init_val']
+            init_loc = molecule['init_loc']
+            if name not in [e.name for e in MoleculeTypes]:
+                raise TypeError(f'Molecule {name} is not implemented yet')
+
+            elif init_loc not in [e.name for e in TissueTypes]:
+                raise TypeError(f'Cannot find lung tissue type {init_loc}')
+
+            molecules.concentration[name][
+                np.where(geometry.lung_tissue == TissueTypes[init_loc].value)
+            ] = init_val
+
         molecules.concentration = np.rec.array(molecules.concentration)
-
-        iron_init_val = self.config.getfloat('iron_init_concentration')
-
-        # TODO initialize in a user/geometry specific way
-        molecules.concentration.iron[:] = iron_init_val
-
-        # molecules.source[:] = 0
 
         return state
 
     def advance(self, state: State, previous_time: float):
         """Advance the state by a single time step."""
         molecules: MoleculesState = state.molecules
+        # grid: RectangularGrid = state.grid
 
-        iron = molecules.concentration.iron
-
-        Molecules.diffuse(iron)
-        Molecules.degrade(iron)
+        tafc = molecules.concentration.tafc
+        self.diffuse(tafc)
+        self.degrade(tafc)
+        # with open('testfile.txt', 'w') as outfile:
+        #     for data_slice in tafc:
+        #         np.savetxt(outfile, data_slice, fmt='%-7.2f')
 
         return state
 
