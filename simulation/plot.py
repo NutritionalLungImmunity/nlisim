c80794b06af12de128586efe81bef7c312bc9285
Brian Helba <brian.helba@kitware.com>
2020-05-14 13:51:24 -0400

    Remove inline "plot_cells_num"

--- simulation/plot.py
+++ simulation/plot.py
@@ -2,14 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_cells_num(timestep: np.ndarray, cells_num: np.ndarray, cell_name: str, path: str):
-    plt.figure()
-    plt.plot(timestep, cells_num)
-    plt.xlabel('time')
-    plt.ylabel(f'{cell_name} number')
-    plt.savefig(path)
-
-
 def plot_cost(module_name: str, cost: np.ndarray, path: str):
     plt.figure()
     timestep = np.arange(len(cost))
