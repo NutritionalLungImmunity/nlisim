5c2c388cb3b1fbd34c49ccbd68bd824f11612720
Joanne Tsai <joanne.tsai@kitware.com>
2020-05-20 12:14:22 -0400

    Add plotpoint module

--- simulation/modules/plotpoint.py
+++ simulation/modules/plotpoint.py
@@ -5,39 +5,61 @@ import numpy as np
 from simulation.module import Module, ModuleState
 from simulation.state import State
 
+
 @attr.s(kw_only=True)
 class PlotPointState(ModuleState):
-    last_plotpoint: float = attr.ib(default=0)
-    num_alive: int = attr.ib(default=0)
+    step_num: int = attr.ib(default=0)
+    fungal_burdens: np.ndarray = attr.ib(factory=lambda: np.zeros(1000, dtype=int))
+    macrophage_counts: np.ndarray = attr.ib(factory=lambda: np.zeros(1000, dtype=int))
+    neutrophil_counts: np.ndarray = attr.ib(factory=lambda: np.zeros(1000, dtype=int))
+    time_steps: np.ndarray = attr.ib(factory=lambda: np.zeros(1000, dtype=float))
+
 
 class PlotPoint(Module):
     name = 'plotpoint'
     defaults = {
-        'plotpoint_interval': '1',
+        'interval': '1',
     }
     StateClass = PlotPointState
 
     def advance(self, state: State, previous_time: float):
-        now = state.time
-        alive_count = len(state.fungus.cells.alive())
+        step_num = state.plotpoint.step_num
 
-        if now - state.plotpoint.last_plotpoint > 1 - 1e-8:
-            state.plotpoint.num_alive = alive_count
-            state.plotpoint.last_plotpoint = now
+        state.plotpoint.time_steps[step_num] = state.time
+        state.plotpoint.fungal_burdens[step_num] = len(state.fungus.cells.alive())
+        state.plotpoint.macrophage_counts[step_num] = len(state.macrophage.cells.alive())
+        state.plotpoint.neutrophil_counts[step_num] = len(state.neutrophil.cells.alive())
+        state.plotpoint.step_num += 1
 
         return state
 
-def create_plot(states):
-    times = []
-    fungal_burden = []
-    for state in states:
-        times.append(state.time)
-        fungal_burden.append(state.plotpoint.num_alive)
+    def finalize(self, state: State):
+        num_steps = state.plotpoint.step_num
+        time_steps = state.plotpoint.time_steps[: num_steps]
+        fungal_burdens = state.plotpoint.fungal_burdens[: num_steps]
+        macrophage_counts = state.plotpoint.macrophage_counts[: num_steps]
+        neutrophil_counts = state.plotpoint.neutrophil_counts[: num_steps]
 
-    fig, ax = plt.subplots()
-    plt.plot(times,fungal_burden)
-    ax.set(xlabel='Time', ylabel='Fungal Burden', title='Fungal Burden vs Time')
-    ax.grid()
+        plt.figure()
+        plt.xlabel('Time')
+        plt.grid(True)
 
-    fig.savefig("fungal_burden_vs_time.png")
-    plt.show()
+        plt.clf()
+        plt.plot(time_steps, fungal_burdens)
+        plt.ylabel('Fungal Burden')
+        plt.title('Fungal Burden vs Time')
+        plt.savefig('fungal_burden_vs_time.png')
+
+        plt.clf()
+        plt.plot(time_steps, macrophage_counts)
+        plt.ylabel('Macrophage Count')
+        plt.title('Macrophage Count vs Time')
+        plt.savefig('macrophage_count_vs_time.png')
+
+        plt.clf()
+        plt.plot(time_steps, neutrophil_counts)
+        plt.ylabel('Neutrophil Count')
+        plt.title('Neutrophil Count vs Time')
+        plt.savefig('neutrophil_count_vs_time.png')
+
+        return state
