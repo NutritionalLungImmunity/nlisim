72073fb0c9dcb08ddb71588d4b74fb80c13e4967
Joanne Tsai <joanne.tsai@kitware.com>
2020-05-20 15:44:34 -0400

    Remove additions made regarding costs

--- simulation/solver.py
+++ simulation/solver.py
@@ -12,10 +12,7 @@ def initialize(state: State) -> State:
     """Initialize a simulation state."""
     for m in state.config.modules:
         with validation_context(f'{m.name} (initialization)'):
-            start = time.clock()
             state = m.initialize(state)
-            end = time.clock()
-            m.add_cost(end - start)
 
     # run validation after all initialization is done otherwise validation
     # could fail on a module's private state before it can initialize itself
@@ -36,10 +33,7 @@ def advance(state: State, target_time: float) -> Iterator[State]:
 
         for m in state.config.modules:
             with validation_context(m.name):
-                start = time.clock()
                 state = m.advance(state, previous_time)
-                end = time.clock()
-                m.add_cost(end - start)
                 attr.validate(state)
 
         yield state
