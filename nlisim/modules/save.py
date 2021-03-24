a566a45a0c00f54ceb530e984324ae84ed0553ee
Adam Knapp <adam.knapp@instanton.org>
2021-03-24 15:31:37 -0400

    Revert "Refactor "file_output" module as "state_output""
    This reverts commit 1e5634bb399f098528a0dd1a1959592d7a064777.
    
    # Conflicts:
    #	config.ini.example
    #	nlisim/modules/save.py
    #	nlisim/modules/state_output.py
    #	notebooks/sample.py.ipynb
    #	simulation/cli.py

---
+++ nlisim/modules/save.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+import attr
+
+from simulation.module import Module, ModuleState
+from simulation.state import State
+
+
+class FileOutput(Module):
+    name = 'file_output'
+    defaults = {'save_interval': '1', 'save_file_name': 'output/simulation-<time>.hdf5'}
+
+    @attr.s(kw_only=True)
+    class StateClass(ModuleState):
+        last_save: float = attr.ib(default=0)
+
+    def advance(self, state: State, previous_time: float) -> State:
+        save_interval = self.config.getfloat('save_interval')
+        save_file_name = self.config.get('save_file_name')
+        now = state.time
+
+        if now - state.file_output.last_save > save_interval - 1e-8:
+            path = Path(save_file_name.replace('<time>', ('%010.3f' % now).strip()))
+            path.parent.mkdir(parents=True, exist_ok=True)
+            state.save(path)
+            state.file_output.last_save = now
+
+        return state
