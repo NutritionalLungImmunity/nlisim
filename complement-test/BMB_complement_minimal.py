cc736eaaae354259f0961d1b168f880969255b91
daniel-a-cruz <56093745+daniel-a-cruz@users.noreply.github.com>
2022-11-23 10:38:37 -0500

    Imported BMB Complement Model
    Set up and imported the (alternative pathway) complement model from https://doi.org/10.1007/s11538-020-00708-z

---
+++ complement-test/BMB_complement_minimal.py
@@ -0,0 +1,38 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+
+import constants
+
+def minimalModel(t, var, con):
+    [C3, C3b, C3bB_c, C3bB_o, C3bBb, FB, FH, C3bH, C3bBbH] = var
+    [k, ks, d, FD, FI] = con
+    
+    dC3 = ks[1] - d[1]*C3 - k[1]*C3 - (k[2]*C3bBb*C3)/(k[3]+C3)
+    dC3b = k[1]*C3 + (k[2]*C3bBb*C3)/(k[3]+C3) - k[4]*C3b*FB + k[5]*C3bB_c + k[6]*C3bBb - k[15]*C3b*FH + k[16]*C3bH + k[21]*C3bBbH
+    dC3bB_c = k[4]*C3b*FB - k[5]*C3bB_c - k[9]*C3bB_c + k[10]*C3bB_o
+    dC3bB_o = k[9]*C3bB_c - k[10]*C3bB_o - (k[7]*FD*C3bB_o)/(k[8] + C3bB_o)
+    dC3bBb = (k[7]*FD*C3bB_o)/(k[8] + C3bB_o) - k[6]*C3bBb - k[25]*C3bBb*FH + k[16]*C3bBbH
+    dFB = ks[2] - d[2]*FB - k[4]*C3b*FB + k[5]*C3bB_c
+    dFH = ks[3] - d[3]*FH - k[15]*C3b*FH + k[16]*C3bH - k[25]*C3bBb*FH + k[16]*C3bBbH + (k[19]*C3bH*FI)/(k[20] + C3bH) + k[21]*C3bBbH
+    dC3bH = k[15]*C3b*FH - k[16]*C3bH - (k[19]*C3bH*FI)/(k[20] + C3bH)
+    dC3bBbH = k[25]*C3bBb*FH - k[16]*C3bBbH - k[21]*C3bBbH
+    
+    return [dC3, dC3b, dC3bB_c, dC3bB_o, dC3bBb, dFB, dFH, dC3bH, dC3bBbH]
+
+const = [[constants.k, constants.ks, constants.d, constants.FD, constants.FI]]
+
+# Initial state of system
+var0 = np.zeros(9)
+
+# C3 initial value
+var0[0] = 6
+
+# FB initial value
+var0[5] = 2
+
+# FH initial value
+var0[6] = 3
+
+t_span = (0.0, 100.0)
+
+result = solve_ivp(minimalModel, t_span, var0, args=const, method='BDF')
