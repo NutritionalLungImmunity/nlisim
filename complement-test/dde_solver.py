46e99911f058fe6f064bec132e3f655a35aff12b
Adam Knapp <adam.knapp@instanton.org>
2023-01-23 10:36:25 -0500

    vectorize, enable jit

--- complement-test/dde_solver.py
+++ complement-test/dde_solver.py
@@ -1,5 +1,6 @@
-from typing import Callable, List, Tuple
+from typing import Callable, List, Tuple, Union
 
+import numba
 from numba import njit
 import numpy as np
 from numpy.linalg import LinAlgError
@@ -14,8 +15,8 @@ from numpy.linalg import LinAlgError
 # (delay values are constants, so they act more like parameters here)
 
 
-@njit
-def index(ts, bound) -> int:
+@njit(inline='always')
+def index(ts: np.ndarray, bound: float) -> int:
     for idx, t in np.ndenumerate(ts):
         if t >= bound:
             return idx[0]
@@ -25,10 +26,39 @@ def index(ts, bound) -> int:
 def make_delay_solver(
     f: Callable[[float, np.ndarray, np.ndarray], np.ndarray],
     jac: Callable[[float, np.ndarray, np.ndarray], np.ndarray],
-    delays: List[float],
+    delays: Union[List[float], np.ndarray],
     implicit_euler: bool = False,
-):
-    # @jit(nopython=True)
+    jit_compile: bool = False,
+) -> Callable[[np.ndarray, Tuple[float, float], float], Tuple[np.ndarray, np.ndarray]]:
+    """
+    Create a vectorizable delay differential equation (DDE) solver for stiff first order DDEs.
+
+    Parameters
+    ----------
+    f: Callable[[float, np.ndarray, np.ndarray], np.ndarray]
+        The function f in y'=f(t,y,y_delay) where y is an n-vector and y_delay is an (d,n) array of
+        d delayed y values.
+    jac: Callable[[float, np.ndarray, np.ndarray], np.ndarray]
+        The Jacobian of f, taking values as (n,n) matrices. Parameters are identical to those of f.
+    delays: Union[List[float], np.ndarray]
+        A list of the (d,) delays used in y_delay.
+    implicit_euler: bool
+        If True, use the implicit Euler scheme to do integration. Otherwise, the solver uses BDF
+        formulas orders 1 through 3. The BDF solver is a higher order solver and may have a higher
+        accuracy, but the implicit Euler scheme is simpler.
+    jit_compile: bool
+        Use Numba's jit compilation to optimize the solver. This imparts a one-time startup cost.
+
+    Returns
+    -------
+    solver: Callable[[np.ndarray, Tuple[float, float], float], Tuple[np.ndarray, np.ndarray]]
+        A function with three parameters, y0, t_span, and an optional dt. Here, y0 is an
+        (m,n) np.ndarray of initial conditions (where n is the dimension of the DDE and m is the
+        number of initial conditions), t_span is a Tuple[float, float] of the time range to
+        integrate over and dt is a float specifying the step size. When dt is not provided, it will
+        be set so that the t_span takes 16 steps.
+    """
+
     def make_delay(ts: np.ndarray, ys: np.ndarray, delay: float, t_current: float) -> np.ndarray:
         """
         Compute delayed values of ys.
@@ -60,7 +90,9 @@ def make_delay_solver(
         remaining_delay = delay - (t_current - ts[idx])
         return ys[idx] * (1 - remaining_delay / dt) + ys[idx - 1] * (remaining_delay / dt)
 
-    # @jit(nopython=True)
+    if jit_compile:
+        make_delay = njit(make_delay, inline='always')
+
     def newton_raphson(
         t_npk: float,
         dt: float,
@@ -88,7 +120,9 @@ def make_delay_solver(
             if np.allclose(correction, 0.0, rtol=1.0e-5, atol=1.0e-8, equal_nan=False):
                 break
 
-    # @jit(nopython=True)
+    if jit_compile:
+        newton_raphson = njit(newton_raphson, inline='always')
+
     def implicit_euler_step(
         t_np1: float,
         dt: float,
@@ -109,14 +143,56 @@ def make_delay_solver(
             ys.shape[1],
         )
 
+    if jit_compile:
+        implicit_euler_step = njit(implicit_euler_step, inline='always')
+
+    def implicit_euler_advance(
+        *,
+        broadcast_shape,
+        broadcast_size,
+        dt,
+        is_broadcast,
+        next_time_idx,
+        ts,
+        ys,
+    ):
+        if is_broadcast:
+            for broadcast_idx_flat in numba.prange(broadcast_size):
+                broadcast_idx = np.unravel_index(broadcast_idx_flat, broadcast_shape)
+                ys_delay = np.stack(
+                    [
+                        make_delay(ts, ys[(np.s_[:], *broadcast_idx)], delay, ts[next_time_idx])
+                        for delay in delays
+                    ],
+                    axis=-1,
+                )
+                implicit_euler_step(
+                    ts[next_time_idx],
+                    dt,
+                    ys[(np.s_[:], *broadcast_idx)],
+                    ys_delay,
+                    next_time_idx,
+                )
+        else:
+            ys_delay = np.stack(
+                [make_delay(ts, ys, delay, ts[next_time_idx]) for delay in delays], axis=-1
+            )
+            implicit_euler_step(ts[next_time_idx], dt, ys, ys_delay, next_time_idx)
+
+    if jit_compile:
+        implicit_euler_advance = njit(implicit_euler_advance, parallel=True, inline='always')
+
     if implicit_euler:
-        # @jit(nopython=True)
+
         def implicit_euler_solver(
-            *,
             y0: np.ndarray,
             t_span: Tuple[float, float],
             dt: float = -1.0,
         ) -> Tuple[np.ndarray, np.ndarray]:
+            is_broadcast = len(y0.shape) >= 2
+            broadcast_shape = y0.shape[:-1]
+            broadcast_size = -1 if not is_broadcast else int(np.prod(broadcast_shape))
+
             # https://en.wikipedia.org/wiki/Backward_Euler_method
             t_start, t_end = t_span
             if dt <= 0.0:
@@ -132,33 +208,41 @@ def make_delay_solver(
             ys = np.zeros((num_datapoints,) + y0.shape, dtype=np.float64)
             ys[0] = y0
 
-            for prev_idx in range(num_datapoints - 2):
+            for prev_time_idx in range(num_datapoints - 2):
                 ################################################################################
                 # implicit Euler step
                 ################################################################################
-                next_idx = prev_idx + 1
-                ts[next_idx] = ts[prev_idx] + dt
-                ys_delay = np.stack(
-                    [make_delay(ts, ys, delay, ts[next_idx]) for delay in delays], axis=-1
+                next_time_idx = prev_time_idx + 1
+                ts[next_time_idx] = ts[prev_time_idx] + dt
+                implicit_euler_advance(
+                    broadcast_shape=broadcast_shape,
+                    broadcast_size=broadcast_size,
+                    dt=dt,
+                    is_broadcast=is_broadcast,
+                    next_time_idx=next_time_idx,
+                    ts=ts,
+                    ys=ys,
                 )
-                implicit_euler_step(ts[next_idx], dt, ys, ys_delay, next_idx)
 
             # there is only one step left, but it might be irregularly spaced
-            ys_delay = np.stack([make_delay(ts, ys, delay, t_end) for delay in delays], axis=-1)
-            implicit_euler_step(
-                t_end,
-                t_end - ts[num_datapoints - 2],
-                ys,
-                ys_delay,
-                num_datapoints - 1,
-            )
             ts[num_datapoints - 1] = t_end
+            implicit_euler_advance(
+                broadcast_shape=broadcast_shape,
+                broadcast_size=broadcast_size,
+                dt=t_end - ts[num_datapoints - 2],
+                is_broadcast=is_broadcast,
+                next_time_idx=num_datapoints - 1,
+                ts=ts,
+                ys=ys,
+            )
 
             return ts, ys
 
+        if jit_compile:
+            implicit_euler_solver = njit(implicit_euler_solver)
+
         return implicit_euler_solver
 
-    # @jit(nopython=True)
     def bdf2_step(
         t_np1: float,
         dt: float,
@@ -182,7 +266,9 @@ def make_delay_solver(
             ys.shape[1],
         )
 
-    # @jit(nopython=True)
+    if jit_compile:
+        bdf2_step = njit(bdf2_step, inline='always')
+
     def bdf3_step(
         t_np1: float,
         dt: float,
@@ -206,13 +292,90 @@ def make_delay_solver(
             ys.shape[1],
         )
 
-    # @jit(nopython=True)
-    def bdf_solver(
+    if jit_compile:
+        bdf3_step = njit(bdf3_step, inline='always')
+
+    def bdf2_advance(
         *,
+        broadcast_shape,
+        broadcast_size,
+        dt,
+        is_broadcast,
+        next_time_idx,
+        ts,
+        ys,
+    ):
+        if is_broadcast:
+            for broadcast_idx_flat in numba.prange(broadcast_size):
+                broadcast_idx = np.unravel_index(broadcast_idx_flat, broadcast_shape)
+                ys_delay = np.stack(
+                    [
+                        make_delay(ts, ys[(np.s_[:], *broadcast_idx)], delay, ts[next_time_idx])
+                        for delay in delays
+                    ],
+                    axis=-1,
+                )
+                bdf2_step(
+                    ts[next_time_idx],
+                    dt,
+                    ys[(np.s_[:], *broadcast_idx)],
+                    ys_delay,
+                    next_time_idx,
+                )
+        else:
+            ys_delay = np.stack(
+                [make_delay(ts, ys, delay, ts[next_time_idx]) for delay in delays], axis=-1
+            )
+            bdf2_step(ts[next_time_idx], dt, ys, ys_delay, next_time_idx)
+
+    if jit_compile:
+        bdf2_advance = njit(bdf2_advance, parallel=True, inline='always')
+
+    def bdf3_advance(
+        *,
+        broadcast_shape,
+        broadcast_size,
+        dt,
+        is_broadcast,
+        next_time_idx,
+        ts,
+        ys,
+    ):
+        if is_broadcast:
+            for broadcast_idx_flat in numba.prange(broadcast_size):
+                broadcast_idx = np.unravel_index(broadcast_idx_flat, broadcast_shape)
+                ys_delay = np.stack(
+                    [
+                        make_delay(ts, ys[(np.s_[:], *broadcast_idx)], delay, ts[next_time_idx])
+                        for delay in delays
+                    ],
+                    axis=-1,
+                )
+                bdf3_step(
+                    ts[next_time_idx],
+                    dt,
+                    ys[(np.s_[:], *broadcast_idx)],
+                    ys_delay,
+                    next_time_idx,
+                )
+        else:
+            ys_delay = np.stack(
+                [make_delay(ts, ys, delay, ts[next_time_idx]) for delay in delays], axis=-1
+            )
+            bdf3_step(ts[next_time_idx], dt, ys, ys_delay, next_time_idx)
+
+    if jit_compile:
+        bdf3_advance = njit(bdf3_advance, parallel=True, inline='always')
+
+    def bdf_solver(
         y0: np.ndarray,
         t_span: Tuple[float, float],
         dt: float = -1.0,
     ) -> Tuple[np.ndarray, np.ndarray]:
+        is_broadcast = len(y0.shape) >= 2
+        broadcast_shape = y0.shape[:-1]
+        broadcast_size = -1 if not is_broadcast else int(np.prod(broadcast_shape))
+
         # https://en.wikipedia.org/wiki/Backward_differentiation_formula
         t_start, t_end = t_span
         if dt <= 0.0:
@@ -233,22 +396,38 @@ def make_delay_solver(
         # begin with an implicit Euler step
         ################################################################################
         # compute t_{n} and t_{n+1}
-        prev_idx = 0
-        next_idx = 1
-        ts[next_idx] = (
+        prev_time_idx = 0
+        next_time_idx = 1
+        ts[next_time_idx] = (
             ts[0] + dt if remaining_datapoints > 1 else t_end
         )  # make sure that we nail the final t, if this is it.
         # solve y_{n+1} = y_n + dt*f(t_{n+1},y_{n+1}) using Newton-Raphson
-        ys_delay = np.stack([make_delay(ts, ys, delay, ts[next_idx]) for delay in delays], axis=-1)
-        implicit_euler_step(ts[next_idx], ts[next_idx] - ts[prev_idx], ys, ys_delay, 1)
+        implicit_euler_advance(
+            broadcast_shape=broadcast_shape,
+            broadcast_size=broadcast_size,
+            dt=dt,
+            is_broadcast=is_broadcast,
+            next_time_idx=1,
+            ts=ts,
+            ys=ys,
+        )
         remaining_datapoints -= 1
 
         # if there is only one step left, and it is irregularly spaced,
         # finish with implicit Euler.
-        if remaining_datapoints == 1 and ts[next_idx] + dt > t_end:
+        if remaining_datapoints == 1 and ts[next_time_idx] + dt > t_end:
+            prev_time_idx = next_time_idx
+            next_time_idx += 1
             ts[-1] = t_end
-            ys_delay = np.stack([make_delay(ts, ys, delay, t_end) for delay in delays], axis=-1)
-            implicit_euler_step(t_end, t_end - ts[next_idx], ys, ys_delay, next_idx + 1)
+            implicit_euler_advance(
+                broadcast_shape=broadcast_shape,
+                broadcast_size=broadcast_size,
+                dt=t_end - ts[prev_time_idx],
+                is_broadcast=is_broadcast,
+                next_time_idx=next_time_idx,
+                ts=ts,
+                ys=ys,
+            )
             remaining_datapoints -= 1
 
         # if this is the end, return results
@@ -258,19 +437,35 @@ def make_delay_solver(
         ################################################################################
         # second step comes from an order 2 BDF method
         ################################################################################
-        prev_idx += 1  # = 1
-        next_idx += 1  # = 2
-        ts[next_idx] = ts[prev_idx] + dt
-        ys_delay = np.stack([make_delay(ts, ys, delay, ts[next_idx]) for delay in delays], axis=-1)
-        bdf2_step(ts[next_idx], dt, ys, ys_delay, next_idx)
+        prev_time_idx += 1  # = 1
+        next_time_idx += 1  # = 2
+        ts[next_time_idx] = ts[prev_time_idx] + dt
+        bdf2_advance(
+            broadcast_shape=broadcast_shape,
+            broadcast_size=broadcast_size,
+            dt=dt,
+            is_broadcast=is_broadcast,
+            next_time_idx=next_time_idx,
+            ts=ts,
+            ys=ys,
+        )
         remaining_datapoints -= 1
 
         # if there is only one step left, and it is irregularly spaced,
         # finish with implicit Euler.
-        if remaining_datapoints == 1 and ts[next_idx] + dt > t_end:
+        if remaining_datapoints == 1 and ts[next_time_idx] + dt > t_end:
+            prev_time_idx = next_time_idx
+            next_time_idx += 1
             ts[-1] = t_end
-            ys_delay = np.stack([make_delay(ts, ys, delay, t_end) for delay in delays], axis=-1)
-            implicit_euler_step(t_end, t_end - ts[next_idx], ys, ys_delay, next_idx + 1)
+            implicit_euler_advance(
+                broadcast_shape=broadcast_shape,
+                broadcast_size=broadcast_size,
+                dt=t_end - ts[prev_time_idx],
+                is_broadcast=is_broadcast,
+                next_time_idx=next_time_idx,
+                ts=ts,
+                ys=ys,
+            )
             remaining_datapoints -= 1
 
         # if this is the end, return results
@@ -281,23 +476,40 @@ def make_delay_solver(
         # third and later steps come from the order 3 BDF method
         ################################################################################
         while remaining_datapoints > 0:
-            prev_idx += 1
-            next_idx += 1
-            ts[next_idx] = ts[prev_idx] + dt
-            ys_delay = np.stack(
-                [make_delay(ts, ys, delay, ts[next_idx]) for delay in delays], axis=-1
+            prev_time_idx += 1
+            next_time_idx += 1
+            ts[next_time_idx] = ts[prev_time_idx] + dt
+            bdf3_advance(
+                broadcast_shape=broadcast_shape,
+                broadcast_size=broadcast_size,
+                dt=dt,
+                is_broadcast=is_broadcast,
+                next_time_idx=next_time_idx,
+                ts=ts,
+                ys=ys,
             )
-            bdf3_step(ts[next_idx], dt, ys, ys_delay, next_idx)
             remaining_datapoints -= 1
 
             # if there is only one step left, and it is irregularly spaced,
             # finish with implicit Euler.
-            if remaining_datapoints == 1 and ts[next_idx] + dt > t_end:
+            if remaining_datapoints == 1 and ts[next_time_idx] + dt > t_end:
+                prev_time_idx = next_time_idx
+                next_time_idx += 1
                 ts[-1] = t_end
-                ys_delay = np.stack([make_delay(ts, ys, delay, t_end) for delay in delays], axis=-1)
-                implicit_euler_step(t_end, t_end - ts[next_idx], ys, ys_delay, next_idx + 1)
+                implicit_euler_advance(
+                    broadcast_shape=broadcast_shape,
+                    broadcast_size=broadcast_size,
+                    dt=t_end - ts[prev_time_idx],
+                    is_broadcast=is_broadcast,
+                    next_time_idx=next_time_idx,
+                    ts=ts,
+                    ys=ys,
+                )
                 remaining_datapoints -= 1
 
         return ts, ys
+
+    if jit_compile:
+        bdf_solver = njit(bdf_solver, parallel=True)
 
     return bdf_solver
